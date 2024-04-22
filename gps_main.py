7f827dab5507e2f6c8c547cbe891a218005eaf00
Saúl Ruiz Fernández <saul.ruiz.30@ull.edu.es>
2024-04-22 17:46:29 +0100

    funciona gps, bme

---
+++ gps_main.py
@@ -0,0 +1,33 @@
+import time
+import board
+import busio
+import adafruit_gps
+import serial
+
+uart = serial.Serial("/dev/serial0", baudrate=9600, timeout=10)
+
+# Creamos una instancia del GPS
+gps = adafruit_gps.GPS(uart, debug=False)
+
+# GGA y RMC (información requerida)
+gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+
+# Frecuencia de actualización (1hz)
+gps.send_command(b"PMTK220,1000")
+
+# Main loop runs forever printing the location, etc. every second.
+def gps_loop(q_gps_out):
+  last_print = time.monotonic()
+  while True:
+    gps.update()
+    current = time.monotonic()
+    # Una vez por segundo, si hay fix, se envían los datos
+    if ((current - last_print) >= 1.0):
+        last_print = current
+        if not gps.has_fix:
+            # Si no hay fix vuelve a intentarlo
+            continue
+         # "a" latitud, "b" longitud, "c" altitud
+        q_gps_out.put("a" + str("{0:.6f}".format(gps.latitude)))
+        q_gps_out.put("b" + str("{0:.6f}".format(gps.longitude)))
+        q_gps_out.put("c" + str(gps.altitude_m))
--- main.py
+++ main.py
@@ -1,1 +1,57 @@
 #!/usr/bin/python
+from multiprocessing import Queue
+from queue import Empty as QueueEmpty
+from threading import Thread
+from time import sleep
+from bme_main import bme_loop
+from gps_main import gps_loop
+
+def unified_loop(q_bme_in, q_gps_in):
+  while True:
+    chain = ''
+    # BME
+    try:
+      data_bme = q_bme_in.get(block=False)
+      if (data_bme[0] == "t"):
+        temperatura = data_bme[1:]
+      elif (data_bme[0] == "h"):
+        humedad = data_bme[1:]
+      elif (data_bme[0] == "p"):
+        presion = data_bme[1:]
+    except QueueEmpty:
+      temperatura = 0
+      humedad = 0
+      presion = 0
+    chain = f'{temperatura},{humedad},{presion},'
+
+    # GPS
+    try:
+      data_gps = q_gps_in.get(block=False)
+      if (data_gps[0] == "a"):
+        latitud = data_gps[1:]
+      elif (data_gps[0] == "b"):
+        longitud = data_gps[1:]
+      elif (data_gps[0] == "c"):
+        altitud = data_gps[1:]
+    except QueueEmpty:
+      latitud = 0
+      longitud = 0
+      altitud = 0
+    chain += f'{latitud},{longitud},{altitud}'
+    
+    # Print the chain
+    print(chain)
+    sleep(1)
+
+# Shared queues
+q_bme = Queue(maxsize=30)
+q_gps = Queue(maxsize=30)
+
+# Threads
+thread_bme = Thread(target=bme_loop, args=(q_bme, ))
+thread_gps = Thread(target=gps_loop, args=(q_gps, ))
+thread_unif = Thread(target=unified_loop, args=(q_bme, q_gps, ))
+
+thread_bme.start()
+thread_gps.start()
+thread_unif.start()
