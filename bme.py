7c5531b8db6dbfb3b8630e35bcda7387ab535f38
Saúl Ruiz Fernández <saul.ruiz.30@ull.edu.es>
2024-04-22 16:41:10 +0100

    first commit

---
+++ bme.py
@@ -0,0 +1,19 @@
+# Temperature, humidity and pressure values, provided by bme680 sensor
+
+from adafruit_bme680 import Adafruit_BME680_I2C
+from board import I2C
+from time import sleep
+
+i2c = I2C()
+bme680 = Adafruit_BME680_I2C(i2c, debug=False)
+TEMPERATURE_OFFSET = -5  # Temperature correction
+
+"""
+Function sending bme data. Inserts in the queue the temperature, humidity and pressure values
+"""
+def bme_loop(q_bme_out):
+    while True:
+        q_bme_out.put("t" + str(bme680.temperature + TEMPERATURE_OFFSET))
+        q_bme_out.put("h" + str(bme680.relative_humidity))
+        q_bme_out.put("p" + str(bme680.pressure))
+        sleep(1)
---
+++ camera.py
@@ -0,0 +1,21 @@
+# Takes a picture every second and saves it to /home/images/image<id>.jpg
+
+import subprocess
+
+i = 0
+try:
+  i = subprocess.check_output('ls -t /home/servet/images | head -n1 | grep -o "0"', shell=True, text=True)
+  i = int(i)
+except:
+  print("No se ha encontrado la imagen")
+    
+
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+while True:
+    i += 1
+    camera.capture('/home/servet/images/image%s.jpg' % i)
+    sleep(10)
