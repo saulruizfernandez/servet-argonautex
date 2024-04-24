fcc2c934644e0237681d66a731c45a1d02d5628f
Saúl Ruiz Fernández <saul.ruiz.30@ull.edu.es>
2024-04-24 18:38:34 +0100

    todo a falta de probar

--- lora_send.py
+++ lora_send.py
@@ -1,13 +1,9 @@
 from SX127x.LoRa import *
-from SX127x.LoRaArgumentParser import LoRaArgumentParser
 from SX127x.board_config import BOARD
 
 from time import sleep
 
-BOARD.setup()
-parser = LoRaArgumentParser("A simple LoRa beacon")
-parser.add_argument('--single', '-S', dest='single', default=False, action="store_true", help="Single transmission")
-parser.add_argument('--wait', '-w', dest='wait', default=1, action="store", type=float, help="Waiting time between transmissions (default is 0s)")
+cadena = ""
 
 # Derived class from LoRa (pyLoRa)
 class MyLoRa(LoRa):
@@ -17,26 +13,32 @@ class MyLoRa(LoRa):
     self.set_mode(MODE.SLEEP)
     self.set_dio_mapping([1,0,0,0,0,0])
 
-  def start(self):
+  def start(self, q_lora_in):
     self.set_mode(MODE.TX)
     while True:
+      try:
+        cadena = q_bme_in.get(block=False)
+      except:
+        pass
       sleep(1)
 
   def on_tx_done(self):
     self.set_mode(MODE.STDBY)
     self.clear_irq_flags(TxDone=1)
     sleep(5) # Sent every 5 seconds
-    self.write_payload(list("hello".encode('ascii')))
-    #print("paquete enviado")
+    self.write_payload(list(cadena.encode('ascii')))
+    print("paquete enviado")
     self.set_mode(MODE.TX)
 
-lora = MyLoRa(verbose=False)
-lora.set_pa_config(pa_select=1)
-lora.set_freq(411.5)
-try:
-  lora.start()
-except:
-  pass
-finally:
-  lora.set_mode(MODE.SLEEP)
-  BOARD.teardown()
+def LoraSend(q_lora_in):
+  BOARD.setup()
+  lora = MyLoRa(verbose=False)
+  lora.set_pa_config(pa_select=1)
+  lora.set_freq(411.5)
+  try:
+    lora.start(q_lora_in)
+  except:
+    pass
+  finally:
+    lora.set_mode(MODE.SLEEP)
+    BOARD.teardown()
--- main.py
+++ main.py
@@ -8,13 +8,14 @@ from time import sleep
 from bme_main import bme_loop
 from gps_main import gps_loop
 from geiger_main import geiger_loop
+from lora_send import LoraSend
 
 # Write header in CSV
 fichero_datos = open('../datos/datos_servet.csv', 'a')
 fichero_datos.write('temperatura,humedad,presion,latitud,longitud,altitud,cpm,nsvh,usvh,ugmmm\n')
 fichero_datos.close()
 
-def unified_loop(q_bme_in, q_gps_in, q_geiger_in):
+def unified_loop(q_bme_in, q_gps_in, q_geiger_in, q_lora_out):
   temperatura = 0
   humedad = 0
   presion = 0
@@ -57,7 +58,7 @@ def unified_loop(q_bme_in, q_gps_in, q_geiger_in):
         pass
     chain += f'{latitud},{longitud},{altitud},'
 
-    # GEIGER
+    # GEIGER and DUST
     for i in range (3):
       try:
         data_geiger = q_geiger_in.get(block=False)
@@ -71,25 +72,30 @@ def unified_loop(q_bme_in, q_gps_in, q_geiger_in):
           dust = data_geiger[1:]
       except QueueEmpty:
         pass
-    chain += f'{cpm},{svh},{msvh},{dust}'
 
+    chain += f'{cpm},{svh},{msvh},{dust}'
     # Save data to CSV
     fichero_datos = open('../datos/datos_servet.csv', 'a')
     fichero_datos.write(chain + '\n')
     fichero_datos.close()
+    # Send data over LoRa
+    q_lora_out.put(chain)
 
 # Shared queues
 q_bme = Queue(maxsize=30)
 q_gps = Queue(maxsize=30)
 q_geiger = Queue(maxsize=30)
+q_lora = Queue(maxsize=30)
 
 # Threads
 thread_bme = Thread(target=bme_loop, args=(q_bme, ))
 thread_gps = Thread(target=gps_loop, args=(q_gps, ))
 thread_geiger = Thread(target=geiger_loop, args=(q_geiger, ))
-thread_unif = Thread(target=unified_loop, args=(q_bme, q_gps, q_geiger, ))
+thread_lora = Thread(target=LoraSend, args=(q_lora, ))
+thread_unif = Thread(target=unified_loop, args=(q_bme, q_gps, q_geiger, q_lora, ))
 
 thread_bme.start()
 thread_gps.start()
 thread_geiger.start()
+thread_lora.start()
 thread_unif.start()
